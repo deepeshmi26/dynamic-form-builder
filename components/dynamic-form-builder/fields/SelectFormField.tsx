0ee086ef934b43a8a4f6867d06b0226c2210c90b
deepeshmi26 <deepesh.mi26@gmail.com>
2025-10-18 18:32:24 +0530

    Added cross icon

--- components/dynamic-form-builder/fields/SelectFormField.tsx
+++ components/dynamic-form-builder/fields/SelectFormField.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { X } from "lucide-react";
 import {
   Select,
   SelectContent,
@@ -8,6 +9,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { FormOption } from "../types";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   value?: string;
@@ -22,21 +24,44 @@ export const SelectFormField = ({
   options,
   placeholder,
 }: Props) => {
+  const handleClear = (e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    onChange?.("");
+  };
+
   return (
-    <Select onValueChange={onChange} value={value as string}>
-      <SelectTrigger className="w-full text-sm sm:text-base !mb-0 text-left">
-        <SelectValue
-          placeholder={placeholder || "Select"}
-          className="truncate text-left"
-        />
-      </SelectTrigger>
-      <SelectContent>
-        {options?.map((opt) => (
-          <SelectItem key={opt.value} value={opt.value} disabled={opt.disabled}>
-            {opt.label}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    <div className="flex items-center gap-0">
+      <Select onValueChange={onChange} value={value as string}>
+        <SelectTrigger className="w-full text-sm sm:text-base !mb-0 text-left">
+          <SelectValue
+            placeholder={placeholder || "Select"}
+            className="truncate text-left"
+          />
+        </SelectTrigger>
+        <SelectContent>
+          {options?.map((opt) => (
+            <SelectItem
+              key={opt.value}
+              value={opt.value}
+              disabled={opt.disabled}
+            >
+              {opt.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      {value && (
+        <Button
+          type="button"
+          variant="ghost"
+          onClick={handleClear}
+          className=" hover:text-red-500 transition-colors absolute right-16"
+          aria-label="Clear selection"
+        >
+          <X className="h-4 w-4 text-gray-400" />
+        </Button>
+      )}
+    </div>
   );
 };
