42ba6306f5b226d3573d0841faf93102d513bdd0
deepeshmi26 <deepesh.mi26@gmail.com>
2025-10-18 18:55:37 +0530

    Revert "Added cross icon"
    This reverts commit 1ebf4e604fc7b1457a6d68721e7966334a2d5e0f.

--- components/dynamic-form-builder/fields/SelectFormField.tsx
+++ components/dynamic-form-builder/fields/SelectFormField.tsx
@@ -8,8 +8,6 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { FormOption } from "../types";
-import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
 
 type Props = {
   value?: string;
@@ -25,37 +23,20 @@ export const SelectFormField = ({
   placeholder,
 }: Props) => {
   return (
-    <div className="relative grid gap-2 space-y-2">
-      <Select onValueChange={onChange} value={value as string}>
-        <SelectTrigger className="w-full text-sm sm:text-base !mb-0 text-left">
-          <SelectValue
-            placeholder={placeholder || "Select"}
-            className="truncate text-left"
-          />
-        </SelectTrigger>
-
-        <SelectContent>
-          {options?.map((opt) => (
-            <SelectItem
-              key={opt.value}
-              value={opt.value}
-              disabled={opt.disabled}
-            >
-              {opt.label}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-      {value && (
-        <Button
-          variant="ghost"
-          className="absolute right-8 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
-          size="icon"
-          onClick={() => onChange?.("")}
-        >
-          <X className="h-4 w-4" />
-        </Button>
-      )}
-    </div>
+    <Select onValueChange={onChange} value={value as string}>
+      <SelectTrigger className="w-full text-sm sm:text-base !mb-0 text-left">
+        <SelectValue
+          placeholder={placeholder || "Select"}
+          className="truncate text-left"
+        />
+      </SelectTrigger>
+      <SelectContent>
+        {options?.map((opt) => (
+          <SelectItem key={opt.value} value={opt.value} disabled={opt.disabled}>
+            {opt.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 };
