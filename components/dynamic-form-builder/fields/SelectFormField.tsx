621c3a52d677e212a71ba507729d6d2eadbb60d5
deepeshmi26 <deepesh.mi26@gmail.com>
2025-10-18 18:42:01 +0530

    Revert "Added cross icon"
    This reverts commit 0ee086ef934b43a8a4f6867d06b0226c2210c90b.

--- components/dynamic-form-builder/fields/SelectFormField.tsx
+++ components/dynamic-form-builder/fields/SelectFormField.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { X } from "lucide-react";
 import {
   Select,
   SelectContent,
@@ -9,7 +8,6 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { FormOption } from "../types";
-import { Button } from "@/components/ui/button";
 
 type Props = {
   value?: string;
@@ -24,44 +22,21 @@ export const SelectFormField = ({
   options,
   placeholder,
 }: Props) => {
-  const handleClear = (e: React.MouseEvent) => {
-    e.preventDefault();
-    e.stopPropagation();
-    onChange?.("");
-  };
-
   return (
-    <div className="flex items-center gap-0">
-      <Select onValueChange={onChange} value={value as string}>
-        <SelectTrigger className="w-full text-sm sm:text-base !mb-0 text-left">
-          <SelectValue
-            placeholder={placeholder || "Select"}
-            className="truncate text-left"
-          />
-        </SelectTrigger>
-        <SelectContent>
-          {options?.map((opt) => (
-            <SelectItem
-              key={opt.value}
-              value={opt.value}
-              disabled={opt.disabled}
-            >
-              {opt.label}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-      {value && (
-        <Button
-          type="button"
-          variant="ghost"
-          onClick={handleClear}
-          className=" hover:text-red-500 transition-colors absolute right-16"
-          aria-label="Clear selection"
-        >
-          <X className="h-4 w-4 text-gray-400" />
-        </Button>
-      )}
-    </div>
+    <Select onValueChange={onChange} value={value as string}>
+      <SelectTrigger className="w-full text-sm sm:text-base !mb-0 text-left">
+        <SelectValue
+          placeholder={placeholder || "Select"}
+          className="truncate text-left"
+        />
+      </SelectTrigger>
+      <SelectContent>
+        {options?.map((opt) => (
+          <SelectItem key={opt.value} value={opt.value} disabled={opt.disabled}>
+            {opt.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 };
